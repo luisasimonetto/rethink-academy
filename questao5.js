a798782ddb0523e4df5ba66975960de63dc2cca5
Luisa Simonetto <77082757+luisasimonetto@users.noreply.github.com>
2022-11-10 17:15:55 -0300

    Update questao5.js

--- questao5.js
+++ questao5.js
@@ -1,13 +1,13 @@
 // Função que adiciona ID
 
 const pessoas = [
-    { name: "Fabiana Araújo", age: 23 },
-    { name: "Gabriel Gomes", age: 25 },
-    { name: "Fernando Henrique", age: 17 },
-    { name: "Ana Luiza", age: 2 },
-    { name: "Geralda do Nascimento", age: 93 },
-    { name: "Miguel Souza", age: 70 },
-    { name: "Antonio Miguel", age: 69 },
+    {name: "Fabiana Araújo", age: 23},
+    {name: "Gabriel Gomes", age: 25},
+    {name: "Fernando Henrique", age: 17},
+    {name: "Ana Luiza", age: 2},
+    {name: "Geralda do Nascimento", age: 93},
+    {name: "Miguel Souza", age: 70},
+    {name: "Antonio Miguel", age: 69},
 ]
 
 function adicionaId() {
